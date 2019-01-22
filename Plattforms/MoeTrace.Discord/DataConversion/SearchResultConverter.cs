1d877edfcacde167154a82bd669184eb86c9aeaa
Neuxz <benedikt.oleksik@dawesys.de>
2019-01-22 10:49:20 +0100

    Prevent Crash.

--- Plattforms/MoeTrace.Discord/DataConversion/SearchResultConverter.cs
+++ Plattforms/MoeTrace.Discord/DataConversion/SearchResultConverter.cs
@@ -12,37 +12,45 @@ namespace MoeTrace.MoeTrace.BotRunner.DataConversion
         public static Embed ConvertResults(this ApiConversion moeapi, SearchResponse resp)
         {
             var builder = new EmbedBuilder();
-            if (resp.docs.Length > 0)
+            try
             {
-                Doc data = resp.docs[0];
-                builder.WithTitle($"I'm {Math.Round(data.similarity, 2)*100}% sure that's **{data.title_english}**")
-                    .WithUrl($"https://anilist.co/anime/{data.anilist_id}")
-                    .WithColor(new Color(0x74110))
-                    .WithTimestamp(DateTimeOffset.FromUnixTimeMilliseconds(1547555778762))
-                    .WithFooter(footer =>
-                    {
-                        footer
-                            .WithText("By Neuxz#6356")
-                            .WithIconUrl("https://github.com/Neuxz.png");
-                    })
-                    .WithImageUrl(moeapi.ImageThumbUrl(resp))
-                    .WithAuthor(author =>
-                    {
-                        author
-                            .WithName("TraceMoe.NET")
-                            .WithUrl("https://github.com/Neuxz/TraceMoe.NET")
-                            .WithIconUrl("https://raw.githubusercontent.com/Neuxz/TraceMoe.NET/master/NuGetIcon.png");
-                    })
+                if (resp.docs.Length > 0)
+                {
+                    Doc data = resp.docs[0];
+                    builder.WithTitle($"I'm {Math.Round(data.similarity, 2) * 100}% sure that's **{data.title_english}**")
+                        .WithUrl($"https://anilist.co/anime/{data.anilist_id}")
+                        .WithColor(new Color(0x74110))
+                        .WithTimestamp(DateTimeOffset.FromUnixTimeMilliseconds(1547555778762))
+                        .WithFooter(footer =>
+                        {
+                            footer
+                                .WithText("By Neuxz#6356")
+                                .WithIconUrl("https://github.com/Neuxz.png");
+                        })
+                        .WithImageUrl(moeapi.ImageThumbUrl(resp))
+                        .WithAuthor(author =>
+                        {
+                            author
+                                .WithName("TraceMoe.NET")
+                                .WithUrl("https://github.com/Neuxz/TraceMoe.NET")
+                                .WithIconUrl("https://raw.githubusercontent.com/Neuxz/TraceMoe.NET/master/NuGetIcon.png");
+                        })
 
-                    .AddField("Name English", data.title_english)
-                    .AddField("Name Original", data.anime)
-                    .AddField("Episode", data.episode)
-                    .AddField("Year", data.season);
+                        .AddField("Name English", data.title_english)
+                        .AddField("Name Original", data.anime)
+                        .AddField("Episode", data.episode)
+                        .AddField("Year", data.season);
+                }
+                else
+                {
+                    builder.WithTitle("**NO matches!**");
+
+                }
             }
-            else
+            catch(Exception ex)
             {
-                builder.WithTitle("**NO matches!**");
-
+                Console.Write(ex);
+                builder.WithTitle("Woops! Something went wrong.");
             }
             return builder.Build();
         }
